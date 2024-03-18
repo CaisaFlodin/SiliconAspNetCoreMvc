562f44574cbf767821a30409fbe44250af975070
Caisa <caisaflodin@hotmail.com>
2024-03-18 11:47:00 +0100

    add authentication middleware

--- Presentation.WebApp/Program.cs
+++ Presentation.WebApp/Program.cs
@@ -1,4 +1,5 @@
 using Infrastructure.Contexts;
+using Infrastructure.Entities;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +13,12 @@ namespace Presentation.WebApp
             builder.Services.AddControllersWithViews();
             builder.Services.AddRouting(x => x.LowercaseUrls = true);
             builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+            builder.Services.AddDefaultIdentity<UserEntity>(x =>
+            {
+                x.User.RequireUniqueEmail = true;
+                x.SignIn.RequireConfirmedAccount = false;
+                x.Password.RequiredLength = 8;
+            }).AddEntityFrameworkStores<DataContext>();
 
             builder.Services.AddScoped<AddressRepository>();
             builder.Services.AddScoped<UserRepository>();
@@ -23,6 +30,7 @@ namespace Presentation.WebApp
             app.UseStatusCodePagesWithReExecute("/error", "?statusCode={0}");
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
